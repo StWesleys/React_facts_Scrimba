0705b6be799e6f0633c3567c7522486428692e48
wyrlsts <79468568+wyrlsts@users.noreply.github.com>
2022-07-22 10:16:12 -0300

    React_facts
    App desenvolvido em React

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function NavBar(){
+    return(
+        <div className="navbar">
+            <figure className="logo">
+                <span></span>
+            </figure>
+                        
+            <h2>React Course - Project 1</h2>
+        </div>
+    )  
+}
