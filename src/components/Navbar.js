ef0c5c857cd04ff412d2ca72d5ffd60300f24380
wyrlsts <79468568+wyrlsts@users.noreply.github.com>
2022-07-27 21:02:41 -0300

    Logo import
    Logo importada

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Logo from 'C:/Users/Júlia/Desktop/React/react_facts/React_facts_Scrimba/src/images/logo.svg'
 
 export default function NavBar(){
     return(
         <div className="navbar">
             <figure className="logo">
-                <span></span>
+                <img src={Logo} alt="logo"/>
             </figure>
                         
             <h2>React Course - Project 1</h2>
